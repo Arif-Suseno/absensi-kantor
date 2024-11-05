92dc1fe81a235f1efa360bef397981c6ab900504
unknown <daffahalim187@gmail.com>
2024-11-05 14:36:40 +0700

    menambahkanlayout

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,20 @@
+<nav>
+    <div class="w-1/5 bg-gray-300 h-screen shadow-md">
+        <div class="p-2 text-center text-lg font-bold border-b flex items-center">
+            <img src="{{ Vite::asset('public/images/logo3.png') }}" alt="" class="w-10 h-10 rounded-full">
+            <h1>GSI</h1>
+        </div>
+        <ul class="ml-6 mt-2">
+            <li class="p-2 hover:bg-gray-400 hover:rounded-lg"> <a href=""> Dashboard Admin</a></li>
+            <li class="p-2 ">Manajemen Absensi</li>
+            <li class="p-2 ">Data Karyawan</li>
+            <li class="p-2 ">Pengajuan Izin/Cuti</li>
+            <li class="p-2 ">Profil Karyawan</li>
+        </ul>
+        <ul class="ml-6 mt-2">
+            <li class="p-2 hover:bg-gray-200 cursor-pointer">Dashboard Karyawan</li>
+            <li class="p-2 hover:bg-gray-200 cursor-pointer">Absensi Harian</li>
+            <li class="p-2 hover:bg-gray-200 cursor-pointer">Riwayat Absensi</li>
+            <li class="p-2 hover:bg-gray-200 cursor-pointer">Pengajuan Izin/Cuti</li>
+        </ul>
+</nav>
