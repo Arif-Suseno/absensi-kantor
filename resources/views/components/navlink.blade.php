48e97d7c3bfca2e046f69b10531da38993804692
Daffa Halim <daffahalim187@gmail.com>
2024-12-06 11:32:29 +0700

    memperbarui tampilan header dan sidebar

--- resources/views/components/navlink.blade.php
+++ resources/views/components/navlink.blade.php
@@ -1,6 +1,6 @@
 <a {{ $attributes }}
     class="
-        {{ $active ? 'bg-gray-400 rounded-full text-white hover:bg-gray-200 hover:text-black' : 'bg-gray-300 hover:bg-gray-200' }}
+        {{ $active ? 'bg-gray-400 rounded-full text-blue-800 hover:bg-gray-500 hover:text-black' : 'bg-gray-400 hover:bg-gray-500' }}
         py-2 px-3 font-normal text-sm xl:text-md 2xl:text-lg  
     "
     aria-current="{{ $active ? 'page' : false }}">
