65b81e20f00d15dcc929f5059a3e888b1eda8749
Aditya Rahman <adit69rahman@gmail.com>
2024-12-19 10:08:39 +0700

    Menambahkan user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -40,6 +40,71 @@ class UserSeeder extends Seeder
             'email' => 'seno@gmail.com',
             'password' => bcrypt('654321'),            
             'role' => 'Karyawan'
-        ]]);
+        ],
+        [
+            'jabatan_id' => '1',
+            'kontrak_id' => '1',
+            'nama' => 'Aditya',
+            'email' => 'a6r@gmail.com',
+            'password' => bcrypt('000000') ,
+            'role' => 'Admin'
+        ],
+        [
+            'jabatan_id' => '1',
+            'kontrak_id' => '1',
+            'nama' => 'Ditya',
+            'email' => 'R9@gmail.com',
+            'password' => bcrypt('111111') ,
+            'role' => 'Admin'
+        ],
+        [
+            'jabatan_id' => '1',
+            'kontrak_id' => '1',
+            'nama' => 'Fani',
+            'email' => 'fani@gmail.com',
+            'password' => bcrypt('123456') ,
+            'role' => 'Admin'
+        ],
+        [
+            'jabatan_id' => '1',
+            'kontrak_id' => '1',
+            'nama' => 'Peni',
+            'email' => 'peni@gmail.com',
+            'password' => bcrypt('280906') ,
+            'role' => 'Karyawan'
+        ],
+        [
+            'jabatan_id' => '1',
+            'kontrak_id' => '1',
+            'nama' => 'Dey',
+            'email' => 'dey@gmail.com',
+            'password' => bcrypt('170706') ,
+            'role' => 'Admin'
+        ],
+        [
+            'jabatan_id' => '1',
+            'kontrak_id' => '1',
+            'nama' => 'Dea',
+            'email' => 'dea@gmail.com',
+            'password' => bcrypt('170706') ,
+            'role' => 'Karyawan'
+        ],
+        [
+            'jabatan_id' => '1',
+            'kontrak_id' => '1',
+            'nama' => 'Daffa halim',
+            'email' => 'dap@gmail.com',
+            'password' => bcrypt('dap123') ,
+            'role' => 'Admin'
+        ],
+        [
+            'jabatan_id' => '1',
+            'kontrak_id' => '1',
+            'nama' => 'Halim',
+            'email' => 'lim@gmail.com',
+            'password' => bcrypt('lim123') ,
+            'role' => 'Karyawan'
+        ],
+    ]);
 }
 }
